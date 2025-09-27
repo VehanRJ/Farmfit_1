ac95f7634837eb334c30ff49f34999a021262eff
Vinay <kumbharVinay89@gmail.com>
2025-09-27 14:35:24 +0530

    added weather

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import Index from "./pages/Index";
 import Login from "./pages/Login";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import LoggedOut from "./components/LoggedOut";
+import LoggedOut from "./components/LoggedOut"
 import Dashboard from "./pages/Dashboard";
 import Upload from "./pages/Upload";
 import NotFound from "./pages/NotFound";
@@ -27,9 +27,8 @@ import Layoutnew from "./components/Layout_new";
 const CropHealth = lazy(() => import("./pages/CropHealth"));
 const LiveAlerts = lazy(() => import("./pages/LiveAlerts"));
 const Weather = lazy(() => import("./pages/Weather"));
-const EnvironmentalConditions = lazy(
-  () => import("./components/EnvironmentalConditions")
-);
+const EnvironmentalConditions = lazy(() => import("./components/EnvironmentalConditions"));
+
 
 const queryClient = new QueryClient();
 
@@ -39,120 +38,20 @@ function AnimatedRoutes() {
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <PageWrapper>
-                <Index />
-              </PageWrapper>
-            </Layout>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <Layout>
-              <PageWrapper>
-                <Login />
-              </PageWrapper>
-            </Layout>
-          }
-        />
-        <Route
-          path="/account-created"
-          element={
-            <PageWrapper>
-              <AccountCreated />
-            </PageWrapper>
-          }
-        />
-        <Route
-          path="/logged-out"
-          element={
-            <PageWrapper>
-              <LoggedOut />
-            </PageWrapper>
-          }
-        />
+        <Route path="/" element={<Layout><PageWrapper><Index /></PageWrapper></Layout>} />
+        <Route path="/login" element={<Layout><PageWrapper><Login /></PageWrapper></Layout>} />
+        <Route path="/account-created" element={<PageWrapper><AccountCreated /></PageWrapper>} />
+        <Route path="/logged-out" element={<PageWrapper><LoggedOut /></PageWrapper>} />
 
-        <Route
-          path="/upload"
-          element={
-            <Layout>
-              <Upload />
-            </Layout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <Layout>
-              <PageWrapper>
-                <About />
-              </PageWrapper>
-            </Layout>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <Layout>
-              <PageWrapper>
-                <Contact />
-              </PageWrapper>
-            </Layout>
-          }
-        />
-        {/* 👇 Dashboard gets macOS-style opening */}
-        <Route
-          path="/dashboard"
-          element={
-            <Layoutnew>
-              <DashboardLayout>
-                <Dashboard />
-              </DashboardLayout>
-            </Layoutnew>
-          }
-        />
-        <Route
-          path="/crop-health"
-          element={
-            <Layoutnew>
-              <DashboardLayout>
-                <CropHealth />
-              </DashboardLayout>
-            </Layoutnew>
-          }
-        />
-        <Route
-          path="/live-alerts"
-          element={
-            <Layoutnew>
-              <DashboardLayout>
-                <LiveAlerts />
-              </DashboardLayout>
-            </Layoutnew>
-          }
-        />
-        <Route
-          path="/weather"
-          element={
-            <DashboardLayout>
-              <Weather />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/EnvironmentalConditions"
-          element={
-            <Layoutnew>
-              <DashboardLayout>
-                <EnvironmentalConditions />
-              </DashboardLayout>
-            </Layoutnew>
-          }
-        />
+        <Route path="/upload" element={<Layout><Upload /></Layout>} />
+        <Route path="/about" element={<Layout><PageWrapper><About /></PageWrapper></Layout>} />
+        <Route path="/contact" element={<Layout><PageWrapper><Contact /></PageWrapper></Layout>} />
+  {/* 👇 Dashboard gets macOS-style opening */}
+  <Route path="/dashboard" element={<Layoutnew><DashboardLayout><Dashboard /></DashboardLayout></Layoutnew>} />
+  <Route path="/crop-health" element={<Layoutnew><DashboardLayout><CropHealth /></DashboardLayout></Layoutnew>} />
+  <Route path="/live-alerts" element={<Layoutnew><DashboardLayout><LiveAlerts /></DashboardLayout></Layoutnew>} />
+  <Route path="/weather" element={<DashboardLayout><Indexcopy /></DashboardLayout>} />
+  <Route path="/EnvironmentalConditions" element={<Layoutnew><DashboardLayout><EnvironmentalConditions /></DashboardLayout></Layoutnew>} />
       </Routes>
     </AnimatePresence>
   );
